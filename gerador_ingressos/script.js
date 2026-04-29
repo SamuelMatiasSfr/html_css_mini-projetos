e3d0a7c4ff9973632877ceeff234033dce42c265
Samuel Matias <samuelmatias754@gmail.com>
2026-04-29 13:42:41 -0300

    Adiciona a imagem do participante no ingresso gerado

--- gerador_ingressos/script.js
+++ gerador_ingressos/script.js
@@ -3,14 +3,18 @@ function gerarCodigo(){
     return codigo;
 }
 
-function gerarIngresso(nome, email, github){
+function gerarIngresso(foto, nome, email, github){
     const ticketSectionTitle = document.getElementById('ticket-section-title');
     const ticketSectionText = document.getElementById('ticket-section-text');
+    
+    const ticketFoto = document.getElementById('ticket-photo');
     const ticketName = document.getElementById('ticket-name');
     const ticketGithub = document.getElementById('github-username');
 
     ticketSectionTitle.textContent = "Parabéns " + nome + "! Seu ingresso está pronto.";
     ticketSectionText.innerHTML = "Vamos enviar o seu ingresso para o email <strong>" + email + "</strong>. Enviaremos atualizações conforme a data do evento se aproxima.";
+    
+    ticketFoto.src = URL.createObjectURL(foto);
     ticketName.textContent = nome;
     ticketGithub.textContent = github;
 
@@ -27,11 +31,20 @@ function processaFormulario(){
     botaoForm.addEventListener('click', function(event){
         event.preventDefault();
 
+        const inputFoto = document.getElementById('foto');
         const inputNome = document.getElementById('nome');
         const inputEmail = document.getElementById('email');
-        const inputGithub = document.getElementById('github');
+        const inputGithub = document.getElementById('github'); 
 
-        gerarIngresso(inputNome.value, inputEmail.value, inputGithub.value);
+        if(
+            inputFoto.value &&
+            inputNome.value.trim() &&
+            inputEmail.value.trim() &&
+            inputGithub.value.trim()
+        ) {
+            const foto = inputFoto.files[0];
+            gerarIngresso(foto, inputNome.value, inputEmail.value, inputGithub.value);
+        }
     });
 }
 
