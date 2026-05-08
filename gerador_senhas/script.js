8f3970f44523aa1ce884a037af8ec2f12029f967
Samuel Matias <samuelmatias754@gmail.com>
2026-05-08 15:26:00 -0300

    Cria funções para atualizar o nível de segurança da senha com base nas checkboxes marcadas

--- gerador_senhas/script.js
+++ gerador_senhas/script.js
@@ -13,35 +13,85 @@ function atualizarTamanhoSenha() {
     const textoTamanhoSenha = document.getElementById("texto-tamanho-senha");
 
     barraTamanhoSenha.addEventListener("input", () => {
-        let valor = barraTamanhoSenha.value;
+        let tamanho = barraTamanhoSenha.value;
         let porcentagem = (barraTamanhoSenha.value / 20) * 100;
 
-        if (valor == 0) {
+        if (tamanho == 0) {
             porcentagem = 0;
-            valor = 0
-        } else if (valor > 0 && valor <= 5) {
+            tamanho = 0
+        } else if (tamanho > 0 && tamanho <= 5) {
             porcentagem = 25;
-            valor = 5;
-        } else if (valor > 5 && valor <= 10) {
+            tamanho = 5;
+        } else if (tamanho > 5 && tamanho <= 10) {
             porcentagem = 50;
-            valor = 10;
-        } else if (valor > 10 && valor <= 15) {
+            tamanho = 10;
+        } else if (tamanho > 10 && tamanho <= 15) {
             porcentagem = 75;
-            valor = 15;
-        } else if (valor > 15) {
+            tamanho = 15;
+        } else if (tamanho > 15) {
             porcentagem = 100;
-            valor = 20;
+            tamanho = 20;
         }
         
-        textoTamanhoSenha.textContent = valor;
-        barraTamanhoSenha.value = valor;
+        textoTamanhoSenha.textContent = tamanho;
+        barraTamanhoSenha.value = tamanho;
         barraTamanhoSenha.style.background = `linear-gradient(to right, #6c996f ${porcentagem}%, #14131b ${porcentagem}%`;
     });
 }
 
+function atualizarNivelSeguranca(numCheckboxesMarcadas) {
+    const textoNivelSeguranca = document.getElementById("texto-nivel-seguranca");
+
+    let codigoCorBarra = "";
+
+    if(numCheckboxesMarcadas === 1) {
+        textoNivelSeguranca.textContent = "Muito Baixo";
+        codigoCorBarra = "#d9534f";
+    } else if (numCheckboxesMarcadas === 2) {
+        textoNivelSeguranca.textContent = "Baixo";
+        codigoCorBarra = "#f0ad4e";
+    } else if (numCheckboxesMarcadas === 3) {
+        textoNivelSeguranca.textContent = "Médio";
+        codigoCorBarra = "#facb68";
+    } else if (numCheckboxesMarcadas === 4) {
+        textoNivelSeguranca.textContent = "Alto";
+        codigoCorBarra = "#6c996f";
+    }
+
+    const barrasSeguranca = document.querySelectorAll(".barra-seguranca");
+    barrasSeguranca.forEach((barra, index) => {
+        if(index < numCheckboxesMarcadas) {
+            barra.style.backgroundColor = codigoCorBarra;
+        } else {
+            barra.style.backgroundColor = "#14131b";
+        }
+    });
+}
+
+function gerarSenha() {
+    const botaoGerarSenha = document.getElementById("gerar-senha");
+    botaoGerarSenha.addEventListener("click", () => {
+        //const tamanho = Number(document.getElementById("texto-tamanho-senha").textContent);
+
+        const incluirMaiusculas = document.getElementById("incluir-maiusculas").checked;
+        const incluirMinusculas = document.getElementById("incluir-minusculas").checked;
+        const incluirNumeros = document.getElementById("incluir-numeros").checked;
+        const incluirSimbolos = document.getElementById("incluir-simbolos").checked;
+
+        let numCheckboxesMarcadas = 0;
+        if(incluirMaiusculas) numCheckboxesMarcadas++;
+        if(incluirMinusculas) numCheckboxesMarcadas++;
+        if(incluirNumeros) numCheckboxesMarcadas++;
+        if(incluirSimbolos) numCheckboxesMarcadas++;
+
+        atualizarNivelSeguranca(numCheckboxesMarcadas);
+    });
+}
+
 function main() {
-    atualizarTamanhoSenha();
     copiarSenha();
+    atualizarTamanhoSenha();
+    gerarSenha();
 }
 
 main();
