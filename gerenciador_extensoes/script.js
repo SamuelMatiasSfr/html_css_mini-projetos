4eb957efb0c5e24c1ffc2e947f76698213f16af3
Samuel Matias <samuelmatias754@gmail.com>
2026-05-16 10:51:15 -0300

    Cria funções que filtram a busca de extensões

--- gerenciador_extensoes/script.js
+++ gerenciador_extensoes/script.js
@@ -18,17 +18,69 @@ function mudarStatusExtensao() {
     toggles.forEach((toggle, index) => {
         toggle.addEventListener("change", () => {
             if(toggle.checked) {
-                extensions[index].dataset.status = "inactive";
-            } else {
                 extensions[index].dataset.status = "active";
+            } else {
+                extensions[index].dataset.status = "inactive";
             }
         });
+    });
+}
+
+function buscarTodasExtensoes() {
+    const extensions = document.querySelectorAll(".article-extension");
+
+    extensions.forEach((extension, index) => {
+        extension.style.display = "flex";
+    });
+}
+
+function buscarExtensoesPorStatus(status) {
+    const extensions = document.querySelectorAll(".article-extension");
+
+    extensions.forEach((extension, index) => {
+        if(extension.dataset.status === status) {
+            extension.style.display = "flex";
+        } else {
+            extension.style.display = "none";
+        }
+    });
+}
+
+function mudarStatusButtons(statusAll, statusActive, statusInactive) {
+    const buttonAll = document.getElementById("button-all");
+    const buttonActive = document.getElementById("button-active");
+    const buttonInactive = document.getElementById("button-inactive");
+    
+    buttonAll.dataset.status = statusAll;
+    buttonActive.dataset.status = statusActive;
+    buttonInactive.dataset.status = statusInactive;
+}
+
+function filtrarExtensoes() {
+    const buttonAll = document.getElementById("button-all");
+    const buttonActive = document.getElementById("button-active");
+    const buttonInactive = document.getElementById("button-inactive");
+
+    buttonAll.addEventListener("click", () => {
+        mudarStatusButtons("active", "inactive", "inactive");
+        buscarTodasExtensoes();
+    });
+
+    buttonActive.addEventListener("click", () => {
+        mudarStatusButtons("inactive", "active", "inactive");
+        buscarExtensoesPorStatus("active");
+    });
+
+    buttonInactive.addEventListener("click", () => {
+        mudarStatusButtons("inactive", "inactive", "active");
+        buscarExtensoesPorStatus("inactive");
     });
 }
 
 function main() {
     removerExtensao();
     mudarStatusExtensao();
+    filtrarExtensoes();
 }
 
 main();
